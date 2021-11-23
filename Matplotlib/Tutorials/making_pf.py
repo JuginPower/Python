d1179da0939957ba360cc032e2a57d271bc3330f
JuginPower <88509346+JuginPower@users.noreply.github.com>
2021-11-23 21:55:48 +0100

    Delete Matplotlib directory

--- Matplotlib/Tutorials/making_pf.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr  3 21:03:37 2021
-
-@author: kraft
-"""
-
-path = "C:/Users/kraft/Desktop/meine_Programme/Python/Anaconda/Workstation/finanzmarkt/indices/Indizes.db"
-
